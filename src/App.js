cc4380a8853f63e5a274bcf58d1900e0a439758d
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-01-10 15:22:16 -0800

    added nav links to sign in and sign up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, NavLink } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import SignUp from './views/SignUp';
@@ -7,6 +7,11 @@ import SignIn from './views/SignIn';
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <NavLink to="/signin">Sign In</NavLink>
+
+        <NavLink to="/signup">Sign Up</NavLink>
+      </header>
       <Switch>
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/signin" component={SignIn} />
