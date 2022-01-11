9a5328bf489d6806c4d4abc77c0c55037a3b4881
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-01-10 18:00:50 -0800

    compressed sign in and sign up into Auth, Added log out

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import { Switch, Route, NavLink } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
-import SignUp from './views/SignUp';
-import SignIn from './views/SignIn';
+import Auth from './views/Auth';
+import { getUser, logout } from './services/users';
+import { useState } from 'react';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+  const logOutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
+
   return (
     <BrowserRouter>
       <header>
-        <NavLink to="/signin">Sign In</NavLink>
-
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/auth">Login Page</NavLink>
       </header>
       <Switch>
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/">
+          {currentUser && (
+            <>
+              <h1>Logged In</h1>
+              <button onClick={logOutUser}>Log Out</button>
+            </>
+          )}
+          {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+        </Route>
       </Switch>
     </BrowserRouter>
   );
